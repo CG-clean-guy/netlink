c36eb6bab9c3fe5ca8138ff75de4289324d5c608
Jason Levy <jasonlevy@admins-MacBook-Pro-4.local>
2023-12-14 08:23:33 -0800

    Changed Success Sound Effect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import PathConverter from "./components/PathConverter";
 
 function App() {
+  const [toggleHelp, setToggleHelp] = useState(false)
+
   return (
     <div className="flex flex-col items-center bg-black/70 h-screen">
-      <header className="text-white font-semibold mb-20">
-        <Navbar />
+      <header className="text-white font-semibold mb-10">
+        <Navbar toggleHelp={toggleHelp} setToggleHelp={setToggleHelp} />
         <div className="flex flex-col justify-center items-center mt-10">
+          <img src="cglogo.png" alt="Logo" className="w-20 h-20 mt-5"/>
           <h1 className="font-bold text-lg m-4">Welcome to PathLink</h1>
+          {toggleHelp && (
           <Hero />
+          )}
         </div>
       </header>
       <div className="flex flex-col justify-center items-center w-screen">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,43 @@ import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const Navbar = () => {
+const Navbar = ({toggleHelp, setToggleHelp}) => {
+
+const handleHelpToggle =() => {
+  setToggleHelp(!toggleHelp)
+}
+
+const playSound = () => {
+  const heySound = new Audio("/hey.aac");
+  heySound.play();
+};
+
   return (
     <>
-      <nav className="fixed top-0 left-0 max-h-12 w-full flex flex-1 flex-row bg-purple-950/30">
+      <nav className="fixed top-0 left-0 max-h-12 w-full flex flex-1 flex-row bg-yellow-500 text-black">
         <div>
-          <div className="h-10 flex flex-1 items-center justify-start mx-3">
-            <img src="logo512.png" alt="logo" className="h-8" />
+          <div className="h-10 flex flex-1 items-center justify-start mx-3" onClick={playSound}>
+            <img src="cglogo.png" alt="logo" className="h-8" />
           </div>
         </div>
-        <ul className="text-sm flex flex-row space-x-2 justify-start items-center">
-          <li>
+        <ul className="text-sm flex flex-row space-x-5 justify-start items-center">
+          <li className="hover:text-gray-500 active:scale-90 transition-all duration-150 ease-in">
             <a href="/">Home</a>
           </li>
-          <li>
+          <li className="hover:text-gray-500 active:scale-90 transition-all duration-150 ease-in">
             <button
               onClick={() => {
                 toast.info("PathLink built by Jason Ross Levy ©️2023");
               }}
             >
               About
+            </button>
+          </li>
+          <li className="hover:text-gray-500 active:scale-90 transition-all duration-150 ease-in">
+            <button
+              onClick={handleHelpToggle}
+            >
+              {toggleHelp? "Minimize Help" : "Help"}
             </button>
           </li>
         </ul>
