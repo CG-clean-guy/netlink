518cdca6ae0274d8cdf249ffbc1eb2da53facf71
Jason Levy <jasonlevy@admins-MacBook-Pro-4.local>
2023-12-19 09:03:59 -0800

    Updated users

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,7 +1,7 @@
 // users.js
 const users = [
     { username: "ray", password: "ray", name: "Ray", user:"raycarranza", guser: "ray@teamchemicalguys.com"},
-    { username: "jose", password: "chepe", name: "Jose", user: "josemartinez", guser:"jose@teamchemicalguys.com" },
+    { username: "jose", password: "chepe", name: "Jose", user: "josevelasco", guser:"jose@teamchemicalguys.com" },
     { username: "joey", password: "thumb", name: "Joey", user: "null", guser: "null"},
     { username: "jason", password: "password", name: "Jason", user: "jasonlevy", guser:"jlevy@chemicalguys.com" },
     { username: "test", password: "test123", name: "Tester", user:"testuser", guser:"test@chemicalguys.com"}
