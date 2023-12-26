233dc75a86ed13f0a0d12b3e20fd83a356947ed7
Jason Levy <jasonlevy@admins-MacBook-Pro-4.local>
2023-12-26 10:27:00 -0800

    Added Henry

--- src/utils/users.js
+++ src/utils/users.js
@@ -5,7 +5,8 @@ const users = [
     { username: "joey", password: "thumb", name: "Joey", user: "null", guser: "null", img:"https://ca.slack-edge.com/T6GNHP7FV-UFW7Y1G6N-27dfe17cf3e2-512"},
     { username: "jason", password: "password", name: "Jason", user: "jasonlevy", guser:"jlevy@chemicalguys.com", img:"https://ca.slack-edge.com/T6GNHP7FV-UT7UJQ9HU-aead9fec7e8c-512"},
     { username: "test", password: "test123", name: "Tester", user:"testuser", guser:"test@chemicalguys.com", img: "https://ca.slack-edge.com/T6GNHP7FV-U059T4WBEKA-gbbac00ef43d-72"},
-    { username: "tony", password: "bologna", name: "Tony Chocolonely", user:"anthonyescobedo", guser:"aescobedo@chemicalguys.com", img: "https://clipground.com/images/animals-with-glasses-clipart-3.png"}
+    { username: "tony", password: "bologna", name: "Tony Chocolonely", user:"anthonyescobedo", guser:"aescobedo@chemicalguys.com", img: "https://clipground.com/images/animals-with-glasses-clipart-3.png"},
+    { username: "henry", password: "powerplay", name: "Henry", user:"henryoliva", guser:"henry@detailgarage.com", img: "https://www.chemicalguys.com/dw/image/v2/BCPT_PRD/on/demandware.static/-/Sites-chemicalguys-master-catalog/default/dw3e463685/hi-res/-0-000000-camohatone.jpg"}
   ];
   
   export default users;
